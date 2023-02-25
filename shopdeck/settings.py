3809e7af10b75555c242a95344c3fe6061120359
R.G <61545240+Rix565@users.noreply.github.com>
2023-02-25 15:37:09 +0100

    whoops

--- shopdeck/settings.py
+++ shopdeck/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'shopdeckdb.middleware.ShopMiddleware'
 ]
 
 ROOT_URLCONF = 'shopdeck.urls'
