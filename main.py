fde2128af719df7d1f443459b8edc45e4162fe53
DoggoITA <98964701+DoggoITA@users.noreply.github.com>
2024-04-20 15:41:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 '''
 SOAP Server
-Made by Let's Shop! 2024
+Made by Let's Shop! 2023
 '''
-print("Shopdeck Server - SOAP XML Services\n\nBy Let's Shop Team 2024")
+print("Shopdeck Server - SOAP XML Services\n\nBy Let's Shop Team 2023")
 print("----------------------------------")
 
 from flask import Flask
-import ecs, ias, cas, cdn
+import ecs, ias, cas, cdn, webcdn
 
 app = Flask(__name__)
 app.register_blueprint(ecs.ecs)
 app.register_blueprint(ias.ias)
 app.register_blueprint(cas.cas)
 app.register_blueprint(cdn.ccs)
+app.register_blueprint(webcdn.cdn)
 
 print("READY!")
