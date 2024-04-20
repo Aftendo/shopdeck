704dfd7321479e27f00ddc1394739a8428775620
DoggoITA <98964701+DoggoITA@users.noreply.github.com>
2024-04-20 17:55:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,6 @@ app.register_blueprint(ecs.ecs)
 app.register_blueprint(ias.ias)
 app.register_blueprint(cas.cas)
 app.register_blueprint(cdn.ccs)
-app.register_blueprint(webcdn.cdn)
+app.register_blueprint(assetcdn.cdn)
 
 print("READY!")
