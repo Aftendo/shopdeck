80dbffe04b706fb1a321687f8dfe5253055442a2
DoggoITA <98964701+DoggoITA@users.noreply.github.com>
2024-04-20 15:42:17 +0200

    Update webcdn.py

--- webcdn.py
+++ webcdn.py
@@ -1,26 +1,20 @@
-from flask import Blueprint, make_response, request
+from flask import Blueprint, send_from_directory
 import os
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shopdeck.settings')
 from django.core.wsgi import get_wsgi_application
+
 application = get_wsgi_application()
 from shopdeckdb.models import *
 from django.core.exceptions import ObjectDoesNotExist
-import os.path
-webcdn_directory = '/webcdn/'
 
-ccs = Blueprint("web", "web")
+webcdn_directory = 'webcdn'
 
-@wcss.route('/webcdn/<path:filename>')
+print("CDN (for images, etc.) Services Starting Up")
+
+ccs = Blueprint("web", __name__)
+
+@ccs.route('/webcdn/<path:filename>')
 def serve_file(filename):
     file_path = os.path.join(webcdn_directory, filename)
-    if os.path.isfile(file_path):
-        return send_from_directory(webcdn_directory, filename)
-    else:
-        return "File not found", 404
-
-@app.route('/')
-def home():
-    return 'CDN (for images .etc) Services Starting Up'
-
-if __name__ == '__main__':
-    app.run()
+    return send_from_directory(webcdn_directory, filename) if os.path.isfile(file_path) else ("File not found", 404)
